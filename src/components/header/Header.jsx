2f911ee41516f86ca22a0b2e871f29ccd251f2fe
Hidnish <hideonishimura@live.it>
2022-08-18 15:27:43 +0200

    Set up fixed navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import './header.css';
 import { BsFillArrowDownCircleFill } from 'react-icons/bs';
 import CTA from './CTA';
-import ME from '../../assets/hideo.jpg';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
 	return (
-		<header>
+		<header id="header">
 			<div className="container header__container">
-				<h5>Hello I'm</h5>
-				<h1>Hideo</h1>
+				<h5>Hey there! this is</h5>
+				<h1 className="title">Hideo Nishimura</h1>
 				<h5 className="text-light">Full Stack developer</h5>
 				<CTA />
 				<div className="flexer">
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
 import './navbar.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiUser, BiBookAlt } from 'react-icons/bi';
+import { TbHeartHandshake } from 'react-icons/tb';
+import { RiContactsBook2Line } from 'react-icons/ri';
+
+import { useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  );
-}
+	const [activeNav, setActiveNav] = useState('#header');
+  
+	return (
+		<nav>
+			<a 
+        href="#header" 
+        onClick={() => setActiveNav('#header')} 
+        className={activeNav === '#header' ? 'active' : ''}
+      >
+				<AiOutlineHome />
+			</a>
+			<a 
+        href="#about" 
+        onClick={() => setActiveNav('#about')} 
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+				<BiUser />
+			</a>
+			<a 
+        href="#experience"
+        onClick={() => setActiveNav('#experience')} 
+        className={activeNav === '#experence' ? 'active' : ''}
+      >
+				<BiBookAlt />
+			</a>
+			<a 
+        href="#services"
+        onClick={() => setActiveNav('#services')} 
+        className={activeNav === '#services' ? 'active' : ''}
+      >
+        <TbHeartHandshake />
+			</a>
+			<a 
+        href="#contacts"
+        onClick={() => setActiveNav('#contact')} 
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
+				<RiContactsBook2Line />
+			</a>
+		</nav>
+	);
+};
 
 export default Navbar;
